3ee2ee134e97494d1ad84ca570520fce944e0cc1
AliRamdhan <aliramdhan4551@gmail.com>
2023-11-21 17:53:19 +0700

    last commit bismillah

--- src/components/ModalHighligt.jsx
+++ src/components/ModalHighligt.jsx
@@ -6,7 +6,7 @@ const ModalHighligt = () => {
   const [openModal, setOpenModal] = useState(true);
   const theme = {
     root: {
-      base: "z-50 h-modal h-screen bg-gray-600 overflow-y-auto overflow-x-hidden md:inset-0 md:h-full",
+      base: "z-50 h-modal h-screen bg-gray-600 overflow-y-auto overflow-x-hidden md:inset-0 md:h-full ",
       show: {
         on: "flex bg-gray-900 bg-opacity-50 dark:bg-opacity-90",
         off: "hidden",
@@ -27,16 +27,16 @@ const ModalHighligt = () => {
     content: {
       base: "relative h-full w-full p-4 md:h-auto",
       inner:
-        "relative rounded-lg shadow bg-[#322557] flex flex-col max-h-[90vh] text-white",
+        "relative rounded-lg shadow bg-[#322557] flex flex-col max-h-[90vh] text-white dark:text-white",
     },
     body: {
-      base: "p-6 flex-1 overflow-auto text-white",
+      base: "p-6 flex-1 overflow-auto ",
       popup: "pt-0",
     },
     header: {
-      base: "flex items-start justify-between rounded-t dark:border-gray-600 border-b p-5",
+      base: "flex items-start justify-between rounded-t border-b p-5",
       popup: "p-2 border-b-0",
-      title: "text-xl font-medium text-gray-900",
+      title: "text-xl font-medium text-white dark:text-white",
       close: {
         base: "ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 dark:hover:text-white",
         icon: "h-5 w-5",
